156d55cd640709d724d48503c8de2ae8a2fd520d
Kenneth Jules <kjules@titansolarpower.com>
2023-04-26 10:55:09 -0400

    Added Hell Property json data on frontend.

--- hell-properties/src/App.js
+++ hell-properties/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./hell_property_data.json";
+import React, { useState } from "react";
 
 function App() {
+  const [properties, setProperties] = useState(data);
+
+  const handleEdit = (propertyId, propertyName, newValue) => {
+    // Find the property in the array and update its value
+    const updatedProperties = properties.map((property) => {
+      if (property.id === propertyId) {
+        // Create a new object with the updated value
+        return { ...property, [propertyName]: newValue };
+      } else {
+        return property;
+      }
+    });
+    // Set the new state with the updated properties
+    setProperties(updatedProperties);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hell Property List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Property ID</th>
+            <th>Address</th>
+            <th>Owner</th>
+            <th>Type</th>
+            <th>Roof Type</th>
+            <th>Roof Angle</th>
+          </tr>
+        </thead>
+        <tbody>
+          {properties.map((property) => (
+            <tr key={property.id}>
+              <td>{property.id}</td>
+              <td>{property.address}</td>
+              <td>{property.owner}</td>
+              <td>{property.type}</td>
+              <td>{property.roofType}</td>
+              <td>{property.roofAngle}</td>
+              <td>
+                <input
+                  type="text"
+                  value={property.value}
+                  onChange={(event) =>
+                    handleEdit(property.id, "value", event.target.value)
+                  }
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
